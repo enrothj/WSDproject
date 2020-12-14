f913652c07d2becfdf583968822b8a94e0795407
enrothj <jani.enroth@yahoo.com>
2020-12-14 20:59:14 +0200

    connectionpool parameters from env vars

--- project/config/config.js
+++ project/config/config.js
@@ -3,11 +3,11 @@ import { Pool } from "../deps.js";
 
 const CONCURRENT_CONNECTIONS = 2;
 const connectionPool = new Pool({
-  hostname: "hattie.db.elephantsql.com",
-  database: "qycqrpwq",
-  user: "qycqrpwq",
-  password: "euMQl-Qnv2K_SBclBq1xpTkChfNPsyC8",// TODO move to .env
-  port: 5432
+  hostname: Deno.env.get('PGHOST'),
+  database: Deno.env.get('PGDATABASE'),
+  user: Deno.env.get('PGUSER'),
+  password: Deno.env.get('PGPASSWORD'),
+  port: Number(Deno.env.get('PGPORT')),
 }, CONCURRENT_CONNECTIONS);
 
 
