971dade89824649969c2a1e1748d3530d18301b7
enrothj <jani.enroth@yahoo.com>
2020-12-14 21:01:01 +0200

    remove unused api stuff

--- project/routes/apis/summaryApi.js
+++ project/routes/apis/summaryApi.js
@@ -1,16 +1,4 @@
-//import * as helloService from "../../services/helloService.js";
 import * as sumService from "../../services/summaryService.js";
-
-const getHello = async({response}) => {
-    response.body = { message: await helloService.getHello() };
-};
-
-const setHello = async({request, response}) => {
-    const body = request.body({type: 'json'});
-    const document = await body.value;
-    helloService.setHello(document.message);
-    response.status = 200;
-};
 
 // Returns a summary of all users' reports for the last 7 days
 const getSummary = async ({response}) => {
