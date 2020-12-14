f4937d0972b40570d6a6318ca646ddf62fb1205c
enrothj <jani.enroth@yahoo.com>
2020-12-14 17:29:23 +0200

    updating summaries to be more user friendly

--- project/routes/controllers/summaryController.js
+++ project/routes/controllers/summaryController.js
@@ -33,13 +33,22 @@ const showWeekSummary = async({render, request, session}) => {
   const body = request.body();
   const params = await body.value;
 
-  const user_id = await getUserId(session);
   const week_input = params.get('week');
   const week = week_input.split("W")[1]; // Gets the week number
 
-  const result = await summaryService.getAveragesWeekForUser(week, user_id);
-
-  render('summarization/averages_week.ejs', {data: result, week: week, user_id: user_id});
+  const data = {
+    user_id: await getUserId(session),
+    week: week,
+  }
+  const result = await summaryService.getAveragesWeekForUser(week, data.user_id);
+  
+  if (Object.keys(result).length === 0) {
+    data.dataFound = false;
+  } else {
+    data.dataFound = true;
+    data.results = result;
+  }
+  render('summarization/averages_week.ejs', data);
 }
 
 // Shows the averages for the requested month for the current user
@@ -47,13 +56,24 @@ const showMonthSummary = async({render, request, session}) => {
   const body = request.body();
   const params = await body.value;
 
-  const user_id = await getUserId(session);
   const month_input = params.get('month');
   const month = month_input.split("-")[1]; // Gets the month number
 
-  const result = await summaryService.getAveragesMonthForUser(month, user_id); 
+  const data = {
+    user_id: await getUserId(session),
+    week: week,
+  }
+  const result = await summaryService.getAveragesMonthForUser(month, data.user_id); 
 
-  render('summarization/averages_month.ejs', {data: result, month: month, user_id: user_id});
+  if (Object.keys(result).length === 0) {
+    data.dataFound = false;
+  } else {
+    data.dataFound = true;
+    data.results = result;
+  }
+
+
+  render('summarization/averages_month.ejs', data);
 }
 
 export { showSummaryForUser, showLandingPage, showWeekSummary, showMonthSummary };
