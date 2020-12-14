dad119a895f814b5904db84d4cb3c4e372d28fed
enrothj <jani.enroth@yahoo.com>
2020-12-14 21:39:52 +0200

    finished api

--- project/routes/apis/summaryApi.js
+++ project/routes/apis/summaryApi.js
@@ -7,15 +7,16 @@ const getSummary = async ({response}) => {
     response.status = 200;
 }
 
-const getDaySummary = async ({params, response}) => { // TODO FIX!!!
-    // Make the date
-    const date = `${params.year}-${params.month}-${params.day}`;
-    console.log(`API request date is ${date}`);
+// Gets a summary for the requested date
+const getDaySummary = async ({params, response}) => {
+    const date = new Date();
+    date.setFullYear(params.year);
+    date.setMonth(params.month-1);
+    date.setDate(params.day); 
 
     const result = await sumService.getAveragesDay(date);
     response.body = result;
     response.status = 200;
 }
-   
-export { getHello, setHello };
+
 export { getSummary, getDaySummary }
